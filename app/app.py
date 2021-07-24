bfadea8f3f6f47d87f020a4ccf1362dc75e4fae6
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-24 11:47:04 -0500

    Update: Create Directory App and app.py

---
+++ app/app.py
@@ -0,0 +1,95 @@
+from typing import List, Dict
+import simplejson as json
+from flask import Flask, request, Response, redirect
+from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+from pprint import  pprint
+
+app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+mysql.init_app(app)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    user = {'username': 'Player Project'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblmlbplayers')
+    result = cursor.fetchall()
+    pprint(result)
+    return render_template('index.html', title='Home', user=user, players=result)
+
+
+@app.route('/view/<int:player_id>', methods=['GET'])
+def record_view(player_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblmlbplayers WHERE id=%s', player_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', player=result[0])
+
+
+@app.route('/edit/<int:player_id>', methods=['GET'])
+def form_edit_get(player_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblmlbplayers WHERE id=%s', player_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', player=result[0])
+
+
+@app.route('/edit/<int:player_id>', methods=['POST'])
+def form_update_post(player_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Name'),
+                 request.form.get('Team'),
+                 request.form.get('Position'),
+                 request.form.get('Height'),
+                 request.form.get('Weight'),
+                 request.form.get('Age'), player_id)
+    sql_update_query = """UPDATE tblmlbplayers t SET t.Name = %s, 
+    t.Team = %s, t.Position = %s, t.Height = 
+    %s, t.Weight = %s, t.Age = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/players/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New Player Form')
+
+
+@app.route('/players/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Name'),
+                 request.form.get('Team'),
+                 request.form.get('Position'),
+                 request.form.get('Height'),
+                 request.form.get('Weight'),
+                 request.form.get('Age'))
+    sql_insert_query = """INSERT INTO 
+    tblmlbplayers (Name, Team, Position, Height, Weight, Age) 
+    VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/delete/<int:player_id>', methods=['POST'])
+def form_delete_post(player_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tblmlbplayers WHERE id = %s """
+    cursor.execute(sql_delete_query, player_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
